d20af338b703daefb72574f5aec35d386e292fd7
Евгений <eugeny1988@gmail.com>
2022-05-22 00:15:30 +0300

    Add all files to ArrayList

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,5 +14,15 @@ public class Main {
        List<String> content11 = Files.readAllLines(file11);
        List<String> content21 = Files.readAllLines(file21);
        List<String> content22 = Files.readAllLines(file22);
+       ArrayList<List<String>> filesContent = new ArrayList<>();
+       filesContent.add(content11);
+       filesContent.add(content21);
+       filesContent.add(content22);
+       for (String s:
+            content11) {
+          if (s.equals("require ‘Folder 1/File 1-1’")){
+             System.out.println("Привет!");
+          }
+       }
    }
 }
