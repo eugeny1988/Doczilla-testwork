0770c5897fb554eb1f6a52c02aa2466f891f4f3c
Евгений <eugeny1988@gmail.com>
2022-05-22 00:51:20 +0300

    Read content to all files

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,30 @@
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-       Path file11 = Path.of("Folder 1/File 1-1.txt");
-       Path file21 = Path.of("Folder 2/File 2-1.txt");
-       Path file22 = Path.of("Folder 2/File 2-2.txt");
-       List<String> content11 = Files.readAllLines(file11);
-       List<String> content21 = Files.readAllLines(file21);
-       List<String> content22 = Files.readAllLines(file22);
-       ArrayList<List<String>> filesContent = new ArrayList<>();
-       filesContent.add(content11);
-       filesContent.add(content21);
-       filesContent.add(content22);
-       for (String s:
-            content11) {
-          if (s.equals("require ‘Folder 1/File 1-1’")){
-             System.out.println("Привет!");
-          }
-       }
-   }
+        Path file11 = Path.of("Folder 1/File 1-1.txt");
+        Path file21 = Path.of("Folder 2/File 2-1.txt");
+        Path file22 = Path.of("Folder 2/File 2-2.txt");
+        ArrayList<Path> files = new ArrayList<>();
+        files.add(file11);
+        files.add(file21);
+        files.add(file22);
+        for (Path loopFile :
+                files) {
+            List<String> loopFileContent = Files.readAllLines(loopFile);
+            for (String s:
+                 loopFileContent) {
+                String[] charArray = s.split(" ");
+                for (int i = 0; i < charArray.length; i++) {
+                    if (charArray[i].equals("require")){
+                        System.out.println(charArray[i+1]);
+                    }
+                }
+            }
+        }
+    }
 }
